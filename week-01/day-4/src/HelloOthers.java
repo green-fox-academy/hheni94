e72fc1e5d330e7f08dddc7e90d37d717c39333e2
Heni Havasi <havasi.heni0@gmail.com>
2019-09-12 15:24:37 +0200

    Second and third exercise added

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Dia!");
+        System.out.println("Hello, Vivi!");
+        System.out.println("Hello, Omar!");
+    }
+}
---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,8 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+
+        System.out.println("Heni Havasi");
+        System.out.println(25);
+        System.out.println(1.68);
+    }
+}
