1800ac2cc6bff8ec051f873318b939e592d771ff
Heni Havasi <havasi.heni0@gmail.com>
2019-09-12 15:37:48 +0200

    Changed Swap.java added

--- week-01/day-4/src/Swap.java
+++ week-01/day-4/src/Swap.java
@@ -1,8 +1,11 @@
 public class Swap {
     public static void main(String[] args) {
 
-        int a = 526;
-        int b = 123;
+        int a = 123;
+        int b = 526;
+        int c = a;
+            a = b;
+            b = c;
 
         System.out.println(a);
         System.out.println(b);
