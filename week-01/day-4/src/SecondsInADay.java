d37653710c811da1190da00723fc9d9a28b4f9f0
Heni Havasi <havasi.heni0@gmail.com>
2019-09-13 10:12:09 +0200

    SecondsInADay.java added

---
+++ week-01/day-4/src/SecondsInADay.java
@@ -0,0 +1,17 @@
+public class SecondsInADay {
+    public static void main(String[] args) {
+
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        int currentHoursInSeconds = (currentHours * 60 * 60);
+        int currentMinutesInSeconds = (currentMinutes * 60);
+        int currentTimeInSeconds = currentSeconds + currentHoursInSeconds + currentMinutesInSeconds;
+
+        int aDayInSeconds = (24 * 60 * 60);
+        int remainingTime = aDayInSeconds + currentTimeInSeconds;
+
+        System.out.println("The remaining seconds from this day is: " + remainingTime);
+    }
+}
