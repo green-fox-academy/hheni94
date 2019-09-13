27810a31f49f7dfacc275fe4787c536cfcc0b223
Heni Havasi <havasi.heni0@gmail.com>
2019-09-13 17:39:33 +0200

    Bmi.java exercise added

---
+++ week-01/day-4/src/Bmi.java
@@ -0,0 +1,8 @@
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        System.out.println("The Body Mass Index (BMI) is: " + (massInKg / (heightInM * heightInM)));
+    }
+}
