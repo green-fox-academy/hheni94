477d9634fdb0f4dcdd01eb7f698892bb37f84353
Heni Havasi <havasi.heni0@gmail.com>
2019-09-13 11:28:50 +0200

    I wont cheat on the exams exercise completed

---
+++ week-01/day-4/src/IWontCheatOnTheExams.java
@@ -0,0 +1,11 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        while (a <= 100) {
+            System.out.println("I won't cheat on the exam!");
+            a ++;
+        }
+    }
+}
