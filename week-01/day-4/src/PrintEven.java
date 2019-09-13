94041cc021808e82482757621696d9997f363942
Heni Havasi <havasi.heni0@gmail.com>
2019-09-13 11:35:32 +0200

    PrintEven exercise completed

---
+++ week-01/day-4/src/PrintEven.java
@@ -0,0 +1,11 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        while (a <= 500) {
+            System.out.println(a);
+            a +=2;
+        }
+    }
+}
