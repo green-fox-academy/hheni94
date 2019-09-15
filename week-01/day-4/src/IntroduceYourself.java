d843ec8ee40e0bcc3f3f52b8563f0ed382d3479e
Heni Havasi <havasi.heni0@gmail.com>
2019-09-15 11:43:18 +0200

    IntroduceYourself changed

--- week-01/day-4/src/IntroduceYourself.java
+++ week-01/day-4/src/IntroduceYourself.java
@@ -1,8 +1,11 @@
 public class IntroduceYourself {
     public static void main(String[] args) {
+        String name = "Heni Havasi";
+        int age = 25;
+        double height = 1.68;
 
-        System.out.println("Heni Havasi");
-        System.out.println(25);
-        System.out.println(1.68);
+        System.out.println("My name is: " + name + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I am " + height + " metres high");
     }
 }
