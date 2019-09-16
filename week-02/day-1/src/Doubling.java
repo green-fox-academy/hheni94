f0854a2ca606b5a3a26aae06636d9a1168f8188a
Heni Havasi <havasi.heni0@gmail.com>
2019-09-16 17:46:44 +0200

    Doubling.java added

---
+++ week-02/day-1/src/Doubling.java
@@ -0,0 +1,16 @@
+public class Doubling {
+    public static void main(String[] args) {
+
+        // - Create an integer variable named `baseNum` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+        // - Print the result of `doubling(baseNum)`
+
+        int baseNum = 123;
+        System.out.println(doubdling(baseNum));
+    }
+
+    public static int doubdling(int number) {
+        number *= 2;
+        return number;
+    }
+}
