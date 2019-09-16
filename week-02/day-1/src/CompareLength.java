3eb7ff19d53287a56d081814a48580bc325f0e4b
Heni Havasi <havasi.heni0@gmail.com>
2019-09-16 12:22:25 +0200

    Week-02 added

---
+++ week-02/day-1/src/CompareLength.java
@@ -0,0 +1,11 @@
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print "p2 is longer" if `p2` has more elements than `p1`
+
+public class CompareLength {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ week-02/day-1/src/Third.java
@@ -0,0 +1,11 @@
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+public class Third {
+    public static void main(String[] args) {
+        int[] q = {4, 5, 6, 7};
+
+        System.out.println("The third element is: " + q[2]);
+    }
+}
