3d645a65e9d9112dbe3e85958fa754aef7bd79ba
Heni Havasi <havasi.heni0@gmail.com>
2019-09-16 12:31:54 +0200

    CompareLength.java added

--- week-02/day-1/src/CompareLength.java
+++ week-02/day-1/src/CompareLength.java
@@ -6,6 +6,14 @@
 
 public class CompareLength {
     public static void main(String[] args) {
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5};
 
+        if (p1.length > p2.length) {
+            System.out.println("p1 is longer");
+        }
+        else {
+            System.out.println("p2 is longer");
+        }
     }
 }
