ca53d604b9c4c761dbeb5691a508e9cb17e82617
Heni Havasi <havasi.heni0@gmail.com>
2019-09-18 12:55:42 +0200

    GoToCenter.java changed

--- week-02/day-3/src/GoToCenter.java
+++ week-02/day-3/src/GoToCenter.java
@@ -12,13 +12,13 @@ public class GoToCenter {
         // and draws a line from that point to the center of the canvas.
         // Draw at least 3 lines with that function using a loop.
 
+        Random random = new Random();
+
         for (int i = 0; i <= 3 ; i++) {
-            Random random = new Random();
             int x = random.nextInt(500);
             int y = random.nextInt(500);
             centerLine(x, y, graphics);
         }
-
     }
 public static void centerLine(int x, int y, Graphics graphics) {
         graphics.drawLine(x, y, WIDTH/2, HEIGHT/2);
