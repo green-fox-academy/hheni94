cadeec4d41510daf39de707655520117886ffe95
Heni Havasi <havasi.heni0@gmail.com>
2019-09-18 11:45:52 +0200

    Diagonals.java added

---
+++ week-02/day-3/src/Diagonals.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Diagonals {
+    public static void mainDraw(Graphics graphics) {
+        // Draw the canvas' diagonals.
+        // If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(0, 0, WIDTH, HEIGHT);
+        graphics.setColor(Color.RED);
+        graphics.drawLine(WIDTH, 0, 0, HEIGHT);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 500;
+    static int HEIGHT = 500;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
