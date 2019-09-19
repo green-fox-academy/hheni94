e26549afe1faa6408d84adbb06fdb3f525402718
Heni Havasi <havasi.heni0@gmail.com>
2019-09-19 16:15:06 +0200

    ConnectTheDots.java added

---
+++ week-02/day-3/src/ConnectTheDots.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ConnectTheDots {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that takes 2 parameters:
+        // An array of {x, y} points and graphics
+        // and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+
+        int [][] pointsOfBox = {{10, 10}, {290,  10}, {290, 290}, {10, 290}, {10, 10}};
+        int [][] points = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+
+        connectTheDots(pointsOfBox, graphics);
+        connectTheDots(points, graphics);
+
+    }
+
+    public static void connectTheDots ( int[][] points, Graphics graphics) {
+        graphics.setColor(Color.GREEN);
+        for (int i = 0; i < points.length-1; i++) {
+            graphics.drawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
