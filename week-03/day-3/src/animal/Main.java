015a6682fcc67a8a55b97154ee20b75f612d87ae
Heni Havasi <havasi.heni0@gmail.com>
2019-09-25 13:18:01 +0200

    Animal exercise done

---
+++ week-03/day-3/src/animal/Main.java
@@ -0,0 +1,12 @@
+package animal;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Animal animal1 = new Animal();
+    animal1.eat();
+    System.out.println(animal1.hunger);
+    animal1.drink();
+    System.out.println(animal1.thirst);
+  }
+}
