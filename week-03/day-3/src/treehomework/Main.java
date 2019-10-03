3b2c1f8e2991580e322483daaeda4b1dc4144e7a
Heni Havasi <havasi.heni0@gmail.com>
2019-10-03 16:03:10 +0200

    TreeHomework task done

---
+++ week-03/day-3/src/treehomework/Main.java
@@ -0,0 +1,11 @@
+package treehomework;
+
+public class Main {
+  public static void main(String[] args) {
+    Tree fir = new Tree("fir", "green", 20, "male");
+    Tree linden = new Tree("linden", "yellow", 15, "male");
+    Tree alder = new Tree("alder", "grey", 8, "female");
+    Tree cherry = new Tree("cherry", "red", 3, "female");
+    Tree abele = new Tree("abele", "orange", 5, "male");
+  }
+}
---
+++ week-03/day-3/src/treehomework/Tree.java
@@ -0,0 +1,15 @@
+package treehomework;
+
+public class Tree {
+  String type;
+  String leafColor;
+  int age;
+  String sex;
+
+  public Tree(String type, String leafColor, int age, String sex) {
+    this.type = type;
+    this.leafColor = leafColor;
+    this.age = age;
+    this.sex = sex;
+  }
+}
