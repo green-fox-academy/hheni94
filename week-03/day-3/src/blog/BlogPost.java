d6349fdc2cad4763ff5ba6aee332be898f6ccdba
Heni Havasi <havasi.heni0@gmail.com>
2019-10-03 15:07:39 +0200

    Blog task done

---
+++ week-03/day-3/src/blog/BlogPost.java
@@ -0,0 +1,18 @@
+package blog;
+
+public class BlogPost {
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+
+  public BlogPost() {
+  }
+}
